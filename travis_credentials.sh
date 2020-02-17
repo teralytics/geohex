3b938d190285f4139bea333b3622be2dcfb112a8
Fabiano Francesconi <fabiano.francesconi@teralytics.ch>
2020-02-17 18:53:32 +0100

    Updated credentials

--- travis_credentials.sh
+++ travis_credentials.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-mkdir -p ~/.bintray
-FILE=~/.bintray/.credentials
-cat <<EOF >$FILE
-realm = Bintray API Realm
-host = api.bintray.com
-user = $BINTRAY_USER
-password = $BINTRAY_API_KEY
-EOF
-
 cat <<EOF >~/.npmrc
 //registry.npmjs.org/:_authToken=$NPMJS_AUTH_TOKEN
 EOF
